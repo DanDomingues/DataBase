292867b773afc193c5924d66f9dbfa28c66a2b3a
Danilo Domingues <dom.dev.contact@gmail.com>
2019-02-18 08:10:06 -0300

    Added curve routines to routine utility

--- Utility/CoroutineUtility.cs
+++ Utility/CoroutineUtility.cs
@@ -15,12 +15,13 @@ public static class CoroutineUtility
         return mono.StartCoroutine(WaitUntil_Routine(func, 0f, action));
     }
 
+
     static IEnumerator WaitUntil_Routine(System.Func<bool> func, float delay, UnityAction action)
     {
         yield return new WaitUntil(func);
         yield return new WaitForSeconds(delay);
 
-        action.Execute();
+        action();
     }
 
     public static void StopCorout(MonoBehaviour mono, Coroutine[] corouts)
@@ -35,6 +36,55 @@ public static class CoroutineUtility
     {
         StopCorout(mono, corouts.ToArray());
     }
+
+    public static IEnumerator CurveRoutine(float duration, AnimationCurve curve, System.Action startAction, System.Action<float, float> midAction, System.Action endAction)
+    {
+        startAction?.Invoke();
+
+        var t = 0f;
+        while(t < 1.0f)
+        {
+            t += Time.deltaTime / duration;
+            t = Mathf.Clamp01(t);
+            midAction?.Invoke(t, curve.Evaluate(t));
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        endAction?.Invoke();
+    }
+    public static IEnumerator CurveRoutine(float duration, AnimationCurve curve, System.Action startAction, System.Action<float, float> midAction)
+    {
+        return CurveRoutine(duration, curve, startAction, midAction, null);
+    }
+    public static IEnumerator CurveRoutine(float duration, AnimationCurve curve, System.Action<float, float> midAction, System.Action endAction)
+    {
+        return CurveRoutine(duration, curve, null, midAction, endAction);
+    }
+    public static IEnumerator CurveRoutine(float duration, AnimationCurve curve, System.Action<float, float> midAction)
+    {
+        return CurveRoutine(duration, curve, null, midAction, null);
+    }
+    public static IEnumerator CurveRoutine(float duration, System.Action startAction, System.Action<float, float> midAction, System.Action endAction)
+    {
+        var curve = new AnimationCurve();
+        curve.AddKey(0f, 0f);
+        curve.AddKey(1f, 1f);
+        
+        return CurveRoutine(duration, curve, null, midAction, null);
+    }
+    public static IEnumerator CurveRoutine(float duration, System.Action<float, float> midAction, System.Action endAction)
+    {
+        return CurveRoutine(duration, () => { }, midAction, endAction);
+    }
+    public static IEnumerator CurveRoutine(float duration, System.Action startAction, System.Action<float, float> midAction)
+    {
+        return CurveRoutine(duration, startAction, midAction, () => { });
+    }
+    public static IEnumerator CurveRoutine(float duration, System.Action<float, float> midAction)
+    {
+        return CurveRoutine(duration, () => { }, midAction, () => { });
+    }
 }
 
 public static class CoroutineUtility_Extensions
@@ -47,4 +97,5 @@ public static class CoroutineUtility_Extensions
     {
         return CoroutineUtility.WaitUntil(mono, func, 0f, action);
     }
+    
 }
