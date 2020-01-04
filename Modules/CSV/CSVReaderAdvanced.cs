91dfeeae583eeffec451970c182be05ccb8eb7ce
dominguesdev <dom.dev.contact@gmail.com>
2020-01-04 18:35:24 -0300

    Added CSV integration framework

---
+++ Modules/CSV/CSVReaderAdvanced.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVReaderAdvanced : CSVReader
+{
+    [SerializeField] int startX;
+    [SerializeField] int startY;
+    [SerializeField] int xLength;
+    [SerializeField] int yLength;
+
+    public void ProcessValues(string serialized)
+    {
+        var grid = SplitCsvGrid(serialized);
+        var values = new string[xLength, yLength];
+
+        for (int j = 0; j < yLength; j++)
+        {
+            for (int i = 0; i < xLength; i++)
+            {
+                if(startX + i >= grid.GetLength(0) || startY + j >= grid.GetLength(1))
+                {
+                    Debug.Log(string.Format("[ERROR: {0}]Attempted to access {1} at a {2} grid", name,
+                        new Vector2Int(startX + i, startY + j), new Vector2Int(grid.GetLength(0), grid.GetLength(1))));
+                    continue;
+                }
+                values[i, j] = grid[startX + i, startY + j];
+            }
+        }
+
+        ProcessValues(values);
+    }
+    public void ProcessFile()
+    {
+        ProcessValues(csvFile.text);
+    }
+
+    protected virtual void ProcessValues(string[,] input)
+    {
+
+    }
+    public virtual void ApplyChanges(bool keepChanges)
+    {
+
+    }
+    public int GetColumnIndex(string[,] source, string key, int row)
+    {
+        for (int i = 0; i < source.GetLength(0); i++)
+        {
+            //Debug.Log($"Checking {key} to {source[i, row]}");
+            if (source[i, row].ToLower() == key.ToLower())
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    [ContextMenu("Set bounds to max")]
+    public void SetBoundsToMax()
+    {
+        var grid = SplitCsvGrid(csvFile.text);
+        xLength = grid.GetLength(0);
+        yLength = grid.GetLength(1);
+        startX = Mathf.Clamp(startX, 0, xLength - 1);
+        startY = Mathf.Clamp(startY, 0, yLength - 1);
+    }
+    [ContextMenu("Process File", false, 900)]
+    public void ProcessFile_Editor()
+    {
+        ProcessFile();
+        KeepChanges(this);
+    }
+
+    protected void KeepChanges(ScriptableObject scriptableObject)
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.SetDirty(scriptableObject);
+#endif
+    }
+}
---
+++ Modules/CSV/CSVUtility.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CSVUtility
+{
+    public static void ParseDamage(string input, out int value)
+    {
+        value = 0;
+        int second = 0;
+        ParseDamage(input, out value, out second);
+    }
+    public static void ParseDamage(string input, out int value, out int secondaryValue)
+    {
+        value = 0;
+        secondaryValue = 0;
+
+        input.Trim('x');
+        if(input.Contains("/"))
+        {
+            var split = input.Split('/');
+            int.TryParse(split[0], out value);
+            int.TryParse(split[1], out secondaryValue);
+        }
+        else
+        {
+            int.TryParse(input, out value);
+        }
+    }
+
+    public static void ParseRange(string input, out int min, out int max)
+    {
+        input = input.Trim(' ');
+        min = 0;
+        max = 0;
+
+        if(input.Contains("--"))
+        {
+
+        }
+        else if(input.Contains("-"))
+        {
+            var split = input.Split('-');
+            int.TryParse(split[0], out min);
+
+            if (split[1].ToLower().Contains("all"))
+                max = 1000000;
+            else
+                int.TryParse(split[1], out max);
+        }
+        else if(input.Contains("to"))
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == 't')
+                {
+                    int.TryParse(input.Substring(0, i), out min);
+                    int.TryParse(input.Substring(i + 2, input.Length - (i + 2)), out max);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            int.TryParse(input, out min);
+            int.TryParse(input, out max);
+        }
+    }
+
+    public static void TryParse(string input, out float value)
+    {
+        input = input.Trim(' ').Replace('.', ',');
+        float.TryParse(input, out value);
+    }
+}
