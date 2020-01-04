237a8cf2d81f2100cf286c1c8e8eae032cd3f7fa
dominguesdev <dom.dev.contact@gmail.com>
2020-01-04 18:37:56 -0300

    Added Miscellaneous classes

---
+++ Misc/AudioClipCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "AudioClipCollection", menuName = "Data/References/Audio Clip Collection")]
+public class AudioClipCollection : ScriptableObject
+{
+    [SerializeField] AudioClip[] audioClips;
+    public AudioClip RandomClip
+    {
+        get
+        {
+            return audioClips.Length > 0 ? audioClips[Random.Range(0, audioClips.Length)] : null;
+        }
+    }
+
+    public static implicit operator AudioClip(AudioClipCollection values)
+    {
+        if (values != null) return values.RandomClip;
+        return null;
+    }
+}
---
+++ Misc/PixelFill.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PixelFill : MonoBehaviour
+{
+    [SerializeField] protected RectTransform rect;
+    [SerializeField] protected Image bgImage;
+    [SerializeField] protected Image fillImage;
+    [SerializeField] int brimWidth;
+    [SerializeField, Range(0f, 1f)] float fillAmount;
+
+#if UNITY_EDITOR
+    [SerializeField] bool updateOnGizmos;
+#endif
+    protected float PixelSize => 1f / (rect.sizeDelta.x);
+
+    public float FillAmount
+    {
+        get { return fillAmount; }
+        set { SetFillAmount(value); }
+    }
+
+    protected virtual void Reset()
+    {
+        rect = GetComponent<RectTransform>();
+        brimWidth = 1;
+
+        var imgs = GetComponentsInChildren<Image>();
+        if (imgs.Length > 0)
+        {
+            bgImage = imgs[0];
+            bgImage.fillMethod = Image.FillMethod.Horizontal;            
+        }
+        if (imgs.Length > 1)
+        {
+            fillImage = imgs[1];
+            bgImage.fillMethod = Image.FillMethod.Horizontal;
+        }
+    }
+
+    public virtual void SetFillAmount(float value)
+    {
+        value = Mathf.Round(value / PixelSize) * PixelSize;
+
+        fillImage.fillAmount = value;
+        bgImage.fillAmount = value + (PixelSize * brimWidth);
+
+        fillAmount = value;
+        //Auto step for killing the brim if the value is 0
+        //if(value >= pixelSize && value <= 1 - pixelSize)
+        //{
+
+        //}
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        if(updateOnGizmos) SetFillAmount(fillAmount);
+    }
+#endif
+}
